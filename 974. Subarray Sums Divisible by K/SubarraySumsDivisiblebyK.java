25acb3dc9a70c627b3b590c193849c8d0812ad9a
vinodhKumar2797 <vinothkumars395@gmail.com>
2025-12-15 14:22:13 -0600

    Subarray Sums Divisible by K

---
+++ 974. Subarray Sums Divisible by K/SubarraySumsDivisiblebyK.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0,1);
+        int count = 0;
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++){
+            sum += nums[i];
+           int rem = sum % k;
+            if (rem < 0) rem += k;   
+            count += map.getOrDefault(rem ,0);
+            map.put(rem, map.getOrDefault(rem,0)+1);
+        }
+
+
+        
+
+        return count;
+
+        
+    }
+}
