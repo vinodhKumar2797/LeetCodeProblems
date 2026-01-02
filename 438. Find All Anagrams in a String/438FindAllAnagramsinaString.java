4cbcc1145cff504b686f04c8341e714516528bb9
vinodhKumar2797 <vinothkumars395@gmail.com>
2026-01-02 15:25:42 -0600

    new changes anagrams

--- 438. Find All Anagrams in a String/438FindAllAnagramsinaString.java
+++ 438. Find All Anagrams in a String/438FindAllAnagramsinaString.java
@@ -1,40 +1,69 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
 
 
 
-        int n = s.length();
-        int pn = p.length();
-        List<Integer> res = new ArrayList<>();
-        if(pn > n){
-            return res;
-        }
-        
-        String array[] = new String[n-pn];
-        HashMap <Character, Integer> pmap = new HashMap<>();
-        HashMap <Character, Integer> smap = new HashMap<>();
-        int count = 0;
-        char pt[] = p.toCharArray();
-        Arrays.sort(pt);
-
-        for(int i = 0; i <= n-pn; i++){
-            String st = s.substring(i,i+pn);
-            char a[] = st.toCharArray();
-            Arrays.sort(a);
-            if(Arrays.equals(a, pt)){
-                res.add(i);
+            HashMap<Character,Integer> store = new HashMap<>();
+            HashMap<Character,Integer> pstore = new HashMap<>();
+            List<Integer> res = new ArrayList<>();
+            int pl = p.length();
+            int sl = s.length();
+            if(pl > sl){
+                return new ArrayList<>();
             }
-        }
-        
+            for(int i = 0; i < pl; i++){
+                pstore.put(p.charAt(i), pstore.getOrDefault(p.charAt(i),0) + 1);
+            }
+            for(int i = 0; i < pl; i++){
+                store.put(s.charAt(i), store.getOrDefault(s.charAt(i) , 0) + 1);
+            }
 
-        
+
+
+            for(int i = 0; i < sl - pl + 1; i++){
+                int flag = 0;
+                for(Map.Entry<Character, Integer> entry : pstore.entrySet())
+                {    
+                    char a = entry.getKey();
+                    int b = entry.getValue();
+                    if(pstore.size() == store.size()){
+                        if(store.containsKey(a) && store.getOrDefault(a,0) == b){
+                            flag = 1;
+                        }else{
+                            flag = 0;
+                            break;
+                        }
+                    }else{
+                        break;
+                    }
+                }
+                if(flag == 1){
+                    res.add(i);
+                }
+                char out = s.charAt(i);
+                int val = store.getOrDefault(out, 0);
+                if(val - 1 > 0){
+                    store.put(out, val - 1);
+                }else{
+                    store.remove(out);
+                }
+                if(i + pl < sl){
+                char in = s.charAt(i + pl);
+                store.put(in , store.getOrDefault(in , 0) + 1);
+                System.out.println(store.size() +" "+ pstore.size());}
+               
+            }
 
     return res;
+
+
+
+    
         
     }
 }
